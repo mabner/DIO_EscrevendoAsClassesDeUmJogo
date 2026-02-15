8728a701bf0e626d47b66154522ead72952ec69c
Marcos Leite <30819867+mabner@users.noreply.github.com>
2026-02-15 00:46:01 -0300

    Uses if/elif to return the attack description

---
+++ main_if_elif.py
@@ -0,0 +1,70 @@
+"""
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada
+"""
+
+
+class Heroi:
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+        if self.tipo == "mago":
+            ataque = "magia"
+        elif self.tipo == "guerreiro":
+            ataque = "espada"
+        elif self.tipo == "monge":
+            ataque = "artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "shuriken"
+        else:
+            ataque = "um ataque desconhecido"
+
+        print(f"o {self.tipo} atacou usando {ataque}")
+
+
+if __name__ == "__main__":
+    herois = [
+        Heroi("Bartyra", 47, "mago"),
+        Heroi("Robervaldo", 43, "guerreiro"),
+        Heroi("Aleera", 27, "monge"),
+        Heroi("Salith", 32, "ninja"),
+    ]
+
+    for heroi in herois:
+        heroi.atacar()
